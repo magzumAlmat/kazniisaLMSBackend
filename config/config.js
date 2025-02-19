ab2c61d8fd6dda04d98795ebde9b6019f6a6647b
Billionare <billionare@mac.local>
2025-02-19 17:19:19 +0500

    asdadsdasddd

--- config/config.js
+++ config/config.js
@@ -1,24 +1,25 @@
-const fs = require('fs')
-module.exports={
-    development:{
-        username:'admin',
-        password:'root',
-        database:'admin',
-        host:'localhost',
-        dialect:'postgres'
-    },
+const fs = require('fs');
+require('dotenv').config(); // Подключаем dotenv для загрузки переменных из .env
 
-    production:{
-        // username:'doadmin',
-        // password:'AVNS_nH1mMs0Lohu1k10zHK3',
-        // database:'defaultdb',
-        // host:'db-postgresql-nyc1-25118-do-user-11457016-0.b.db.ondigitalocean.com',
-        // dialect:'postgres',
-        // port:25060,
-        // dialectOptions: {
-        //     ssl: {
-        //       ca: fs.readFileSync('config/ca-certificate.crt')
-        //     },
-        // },
-    }
-}
+module.exports = {
+    development: {
+        username: process.env.DEV_DB_USERNAME,
+        password: process.env.DEV_DB_PASSWORD,
+        database: process.env.DEV_DB_NAME,
+        host: process.env.DEV_DB_HOST,
+        dialect: process.env.DEV_DB_DIALECT,
+    },
+    production: {
+        username: process.env.PROD_DB_USERNAME,
+        password: process.env.PROD_DB_PASSWORD,
+        database: process.env.PROD_DB_NAME,
+        host: process.env.PROD_DB_HOST,
+        port: parseInt(process.env.PROD_DB_PORT, 10), // Преобразуем порт в число
+        dialect: process.env.PROD_DB_DIALECT,
+        dialectOptions: {
+            ssl: {
+                ca: fs.readFileSync('config/ca-certificate.crt'), // Сертификат остается, если нужен
+            },
+        },
+    },
+};
